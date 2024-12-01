7606556400a8ee28fe589ae0b48ef9b58af126b8
Aleks <aleksxx98@gmail.com>
2024-12-01 11:10:36 +0200

    Initial commit

---
+++ 1/1.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+fetch('https://adventofcode.com/2024/day/1/input', {
+  headers: {
+    cookie: process.env.ID
+  }
+})
+  .then(res => res.text())
+  .then(data => {
+    let input = data.split('\n').filter(val => val)
+
+    console.log(input)
+  });
